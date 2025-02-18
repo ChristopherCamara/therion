b5bf5d2260034c2a0646e1aab2d6efc414867819
Christopher Camara <chrisc2774@gmail.com>
2025-02-18 17:23:14 -0500

    commiting this terrible progress

--- main.go
+++ main.go
@@ -2,23 +2,19 @@ package main
 
 import (
 	"embed"
-	"html/template"
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"text/template"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 const BASE_DIR = ".therion"
+const REPLICA_FILE = "replica.txt"
 const DAILY_DIR = "daily"
-
-type operation string
-
-const ()
-
-type file struct {
-	path string
-	data []byte
-}
 
 type TemplateFields struct {
 	Name string
@@ -30,15 +26,44 @@ var templatesFS embed.FS
 
 func main() {
 	args := os.Args[1:]
-
+	var ft FileTree
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		panic(err)
 	}
 	baseDir := filepath.Join(homeDir, BASE_DIR)
-	err = os.MkdirAll(baseDir, os.ModePerm)
+	ft.AddDirectory(baseDir)
+
+	fmt.Println(ft)
+
+	replicaFile, err := os.OpenFile(filepath.Join(baseDir, REPLICA_FILE), os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		panic(err)
+	}
+	defer replicaFile.Close()
+	replicaInfo, err := replicaFile.Stat()
+	if err != nil {
+		panic(err)
+	}
+	var replicaID uuid.UUID
+	if replicaInfo.Size() == 0 {
+		replicaID, err = uuid.NewV7()
+		if err != nil {
+			panic(err)
+		}
+		size, err := replicaFile.WriteString(replicaID.String())
+		if err != nil || size != len(replicaID.String()) {
+			panic("error writing replicaID to file")
+		}
+	} else {
+		id, err := io.ReadAll(replicaFile)
+		if err != nil {
+			panic(err)
+		}
+		replicaID, err = uuid.ParseBytes(id)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	templates, err := template.ParseFS(templatesFS, "templates/*.md")
@@ -48,8 +73,9 @@ func main() {
 
 	for _, arg := range args {
 		if arg == "--today" {
-			today := time.Now()
-			name := ""
+			dailyDir := filepath.Join(baseDir, DAILY_DIR)
+			ft.AddDirectory(dailyDir)
+			name := filepath.Join(dailyDir, fmt.Sprintf("%s.md", time.Now().Format(time.DateOnly)))
 			file, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE, os.ModePerm)
 			if (err) != nil {
 				panic(err)
@@ -66,7 +92,8 @@ func main() {
 					panic(err)
 				}
 			}
-		} // else if arg == "--sync" {
+		}
+		//    else if arg == "--sync" {
 		//		s3 := NewS3Sync()
 		//		sync(baseDir, s3)
 		//	} else {
